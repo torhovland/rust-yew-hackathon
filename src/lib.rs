b53aa79744efe9ed76e1ba33bdef1ce8e6aeb75f
Tor Hovland <tor.hovland@bekk.no>
2020-02-18 22:51:58 +0100

    Split the yew code into an app module.

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,8 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-use yew::{html, Callback, ClickEvent, Component, ComponentLink, Html, ShouldRender};
+use app::App;
 
-struct App {
-    clicked: bool,
-    onclick: Callback<ClickEvent>,
-}
-
-enum Msg {
-    Click,
-}
-
-impl Component for App { 
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        App {
-            clicked: false,
-            onclick: link.callback(|_| Msg::Click),
-        }
-    }
-
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::Click => {
-                self.clicked = true;
-                true // Indicate that the Component should re-render
-            }
-        }
-    }
-
-    fn view(&self) -> Html {
-        let button_text = if self.clicked { "Clicked!" } else { "Click me!" };
-
-        html! {
-            <button onclick=&self.onclick>{ button_text }</button>
-        }
-    }
-}
+mod app;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
