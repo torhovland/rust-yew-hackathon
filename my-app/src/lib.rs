2a33a1afa833930bd1cf2db535be6dd34001f8cd
Tor Hovland <tor.hovland@bekk.no>
2020-02-18 22:21:06 +0100

    Insert the yew sample.

--- my-app/src/lib.rs
+++ my-app/src/lib.rs
@@ -1,6 +1,44 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
+use yew::{html, Callback, ClickEvent, Component, ComponentLink, Html, ShouldRender};
 
+struct App {
+    clicked: bool,
+    onclick: Callback<ClickEvent>,
+}
+
+enum Msg {
+    Click,
+}
+
+impl Component for App { 
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        App {
+            clicked: false,
+            onclick: link.callback(|_| Msg::Click),
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Click => {
+                self.clicked = true;
+                true // Indicate that the Component should re-render
+            }
+        }
+    }
+
+    fn view(&self) -> Html {
+        let button_text = if self.clicked { "Clicked!" } else { "Click me!" };
+
+        html! {
+            <button onclick=&self.onclick>{ button_text }</button>
+        }
+    }
+}
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -21,7 +59,9 @@ pub fn main_js() -> Result<(), JsValue> {
 
 
     // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    console::log_1(&JsValue::from_str("Hello worlds!"));
+
+    yew::start_app::<App>();
 
     Ok(())
 }
